d0faaf79d37c218ef48a63f6785340d20dac3078
Dima Yeremenko <eremenk.dima@gmail.com>
2015-01-29 21:10:48 +0200

    Add some classes BL, DAL, WCF

---
+++ UNO_WCF_Server/DAL/Model/Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Model
+{
+    public class Card
+    {
+        
+        public int id { get; set; }
+
+        public string type { get; set; }
+
+        public string name { get; set; }
+
+        public string item { get; set; }
+
+        public string color { get; set; }
+
+        public string info { get; set; }
+    }
+}
---
+++ UNO_WCF_Server/DAL/Model/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Model
+{
+    public  class User
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string surname { get; set; }
+
+        public string email { get; set; }
+
+        public string passwoed { get; set; }
+
+        public int age { get; set; }
+
+        public string male { get; set; }
+    }
+}
